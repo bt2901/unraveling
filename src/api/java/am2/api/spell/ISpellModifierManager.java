b27fe5c41e4378e0b25d5ecdebac6d021b82733e
Victor Bulatov.co <bt.uytya@gmail.com>
2017-06-18 18:33:57 +0300

    'cleanup'

--- src/api/java/am2/api/spell/ISpellModifierManager.java
+++
@@ -1,8 +0,0 @@
-package am2.api.spell;
-
-import am2.api.spell.component.interfaces.ISpellModifier;
-
-public interface ISpellModifierManager {
-	public int registerSpellModifier(ISpellModifier modifier, String modifierName);
-	public ISpellModifier getSpellModifier(int modifierID);
-}
